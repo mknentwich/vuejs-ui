33d2978ad45b2466ffb91e935a6b1b70cf140b01
Peter Oberhauser <peter.oberhauser@univie.ac.at>
2021-02-28 13:43:21 +0100

    show delivery costs

--- src/components/shop/OrderItemsConfirmation.vue
+++ src/components/shop/OrderItemsConfirmation.vue
@@ -8,14 +8,25 @@
       <v-col cols="2">
         {{ orderItem.quantity }} x
       </v-col>
-      <v-col cols="6">
+      <v-col cols="7">
         {{ orderItem.title }} ({{ orderItem.instrumentation }})
       </v-col>
-      <v-col cols="4" class="text-right">
+      <v-col cols="3" class="text-right">
         je {{ (orderItem.price/100).toLocaleString('de-DE', {minimumFractionDigits: 2}) }} €
       </v-col>
     </v-row>
-    <v-divider></v-divider>
+    <v-row class="primary--text text-body-1 primaryAccent mt-2">
+      <v-divider></v-divider>
+      <v-col cols="8" >
+        <v-icon class="mr-2">mdi-truck</v-icon>
+        <span class="font-weight-black mr-2">Liefergebühr</span>
+        <span class="text-caption">{{ orderConfirmation.identity.address.state.name }}</span>
+      </v-col>
+      <v-col cols="4" class="text-right font-weight-black">
+        {{ (orderConfirmation.identity.address.state.zone.shipping/100).toLocaleString('de-DE', {minimumFractionDigits: 2}) }} €
+      </v-col>
+      <v-divider></v-divider>
+    </v-row>
     <v-row class="primary--text text-body-1">
       <v-col cols="8" class="font-weight-black">
         Gesamtbetrag inkl. Lieferkosten
